759e0ff0ee553191d33133ab53d02ef6a51395bf
ColdCipri <colder.ciprian@gmail.com>
2019-06-27 12:55:09 +0300

    project

---
+++ ExamWP-ASP/index.aspx
@@ -0,0 +1,137 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="ExamWP_ASP.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Exam</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div><asp:HiddenField ID="hiddenfieldID" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label Text="Name" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="textName" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Professor" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="textProfessor" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Duration" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="textDuration" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Button Text="Save" ID="buttonSave" runat="server" OnClick="buttonSave_Click" />
+                        <asp:Button Text="Clear" ID="buttonClear" runat="server" OnClick="buttonClear_Click" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Label Text="" ID="labelSuccessMessage" runat="server" Forecolor="Green" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Label Text="" ID="labelErrorMessage" runat="server" Forecolor="Red" />
+                    </td>
+                </tr>
+            </table>
+
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label Text="CourseID" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="TextBoxCourseID" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Restriction" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="TextBoxRestriction" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Button Text="Save" ID="button1" runat="server" OnClick="buttonSaveRestriction_Click" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Label Text="" ID="label1" runat="server" Forecolor="Green" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Label Text="" ID="label2" runat="server" Forecolor="Red" />
+                    </td>
+                </tr>
+            </table>
+            
+            <br />
+            <br />
+            <table>
+                <tr>
+                    <td colspan="2">
+                        <asp:Label ID="LabelSearch" runat="server" Text="Courses :"></asp:Label>
+                    </td>
+
+                    <td colspan="2">
+                        <asp:DropDownList ID="CourseList" runat="server" AppendDataBoundItems="true">
+                        </asp:DropDownList>
+                    </td>
+
+                </tr>
+            </table>
+            
+            <asp:GridView ID="gvName" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Professor" HeaderText="Professor" />
+                    <asp:BoundField DataField="Duration" HeaderText="Duration" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton Text="Select" ID="linkSelect" CommandArgument='<%# Eval("ID") %>' runat="server" OnClick="linkSelect_OnClick" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ExamWP-ASP/index.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace ExamWP_ASP
+{
+    public partial class index : System.Web.UI.Page
+    {
+        string conn = @"Server=localhost;Database=webdb;Uid=root;";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Clear();
+                GridFill();
+            }
+        }
+
+        protected void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection sqlCon = new MySqlConnection(conn))
+                {
+                    sqlCon.Open();
+                    MySqlCommand sqlCmd = new MySqlCommand("AddOrEditCourse", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("_ID", Convert.ToInt32(hiddenfieldID.Value == "" ? "0" : hiddenfieldID.Value));
+                    sqlCmd.Parameters.AddWithValue("_Name", textName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_Professor", textProfessor.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_Duration", Convert.ToInt32(textDuration.Text.Trim()));
+                    sqlCmd.ExecuteNonQuery();
+
+                    GridFill();
+                    Clear();
+
+                    labelSuccessMessage.Text = "Submitted Sucessfully!";
+                }
+            }
+            catch (Exception ex)
+            {
+                labelErrorMessage.Text = ex.Message;
+            }
+
+        }
+        protected void buttonSaveRestriction_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection sqlCon = new MySqlConnection(conn))
+                {
+                    sqlCon.Open();
+                    MySqlCommand sqlCmd = new MySqlCommand("AddOrEditRestriction", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("_ID", TextBoxCourseID.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_Restriction", TextBoxRestriction.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+
+                    GridFill();
+                    Clear();
+
+                    label1.Text = "Submitted Sucessfully!";
+                }
+            }
+            catch (Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+
+        }
+        
+        private void LoadCourses()
+        {
+            using (MySqlConnection sqlCon = new MySqlConnection(conn))
+            {
+                DataTable courses = new DataTable();
+                sqlCon.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT name FROM courses", conn);
+                adapter.Fill(courses);
+
+                CourseList.DataSource = courses;
+                CourseList.DataTextField = "name";
+                CourseList.DataValueField = "name";
+                CourseList.DataBind();
+            }
+        }
+
+
+        protected void linkSelect_OnClick(object sender, EventArgs e)
+        {
+            int ID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+
+            using (MySqlConnection sqlCon = new MySqlConnection(conn))
+            {
+                sqlCon.Open();
+                MySqlDataAdapter sqlDataAdapter = new MySqlDataAdapter("coursesviewbyid", sqlCon);
+                sqlDataAdapter.SelectCommand.Parameters.AddWithValue("_ID", ID);
+                sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                textName.Text = dataTable.Rows[0][1].ToString();
+                textProfessor.Text = dataTable.Rows[0][2].ToString();
+                textDuration.Text = dataTable.Rows[0][3].ToString();
+
+                hiddenfieldID.Value = dataTable.Rows[0][0].ToString();
+
+                buttonSave.Text = "Update";
+            }
+        }
+
+        protected void buttonClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void GridFill()
+        {
+            using (MySqlConnection sqlCon = new MySqlConnection(conn))
+            {
+                sqlCon.Open();
+                MySqlDataAdapter sqlDataAdapter = new MySqlDataAdapter("coursesviewall", sqlCon);
+                sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                gvName.DataSource = dataTable;
+                gvName.DataBind();
+            }
+        }
+
+        void Clear()
+        {
+            hiddenfieldID.Value = "";
+            textName.Text = textProfessor.Text = textDuration.Text = "";
+            buttonSave.Text = "Save";
+            labelErrorMessage.Text = labelSuccessMessage.Text = "";
+        }
+    }
+}
